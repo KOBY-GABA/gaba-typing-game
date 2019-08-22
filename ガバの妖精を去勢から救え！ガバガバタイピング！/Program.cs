d21c0401c293bff6825471d70c64e3edf56a614d
KOBY <terrarian0518@gmail.com>
2019-08-22 14:03:54 +0900

    first commit

---
+++ ガバの妖精を去勢から救え！ガバガバタイピング！/Program.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ガバの妖精を去勢の危機から救え_ガバガバタイピング_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chinko=3;
+            string[] koby = new string[]
+            {
+                "                                                 \n"+
+                "                                                 \n"+
+                "                                                 \n"+
+                "       `                                         \n"+
+                "    `                                            \n"+
+                "          `                                      \n"+
+                "                                                 \n"+
+                "    `                                            \n"+
+                "       `  `                                      \n"+
+                "         `                                       \n"+
+                "  `                                              \n"+
+                "     `                                           \n"+
+                "         dppbpbppbpbp`    dbpbpbppbpbppp\\ ppppbp \n"+
+                "        .bbppbpbppbpp++&aQbpppbpbppbpbpb:.ppbpppW\n"+
+                "        dbpbppbpbpppppbbbbpbpppbpbppbppppbpbpbpbW\n"+
+                "  `   `.pppbpppbpbpbpppppppbbpbppbppbpbppbppbpppW\n"+
+                "      .bbpbpbpbpppbpbbpbppbppbppbpWkkkbpbppbppbpW\n"+
+                "    ` JbpbpppbNNNNNgkbppbppbppbWNMM#\"MMMNkpbpbppW\n"+
+                "    .XpbppbpWNMMMMHHMMNpbppbbNMM\"!     .WMNppbppW\n"+
+                "    dppbppbNM#^      dMHpbppMM]          ?MNbbpbW\n"+
+                "   .bpppbWMM$        dMdpbpbMMN,         .M#ppppW\n"+
+                "    WbppbMM%        .MMpbppbbMMMNJ.     .dMNpbppW\n"+
+                "    ,bpbbMMb      .MMMpbppbppbpbMMMN&..&MMHppbpbW\n"+
+                "    dpbppbHMMNg++kMMbppbpbppbppbpbbHMMMMHbppbpppW\n"+
+                "    ppppbppppWHMMHbbbpppbppbppbppppppppbpppbpbppW\n"+
+                "    WpbppbppbppbppbpbbpbppbppbpbpbpbbpbpbpbppbpbW\n"+
+                "     .4pbppbpppbpbpbppbpbppbpbppbpbppbpbppbppbppW\n"+
+                "       ?WppbpbppbpbpbppbppbppbpbppbpbppbppbppWY!,\n"+
+                "         _7UbpbppppbpbppbppbppbppbppbppbpbV=`   .\n"+
+                "               ?74bppbppbppbppbpbppbpbppb)      .\n",
+
+
+
+                "                 .Wpbpk,                         \n"+
+                "                .bpppbp$                         \n"+
+                "               .bpbpbpp\\                         \n"+
+                "       `      .WppbppbW`                         \n"+
+                "    `         dbpppbpbf   `                      \n"+
+                "          `  dbpbpbppp%                          \n"+
+                "           .bbpbppbppK    `                      \n"+
+                "    `      ,bppbppbpbf                           \n"+
+                "       `  `dbppbpbpbb$                           \n"+
+                "         `(ppbppbpppb\\                           \n"+
+                "  `      .pppbppbpbpp:  `                        \n"+
+                "     `   dppbpbppbpbp`    dbpbpbppbpbppp\\ ppppbp \n"+
+                "        .bbppbpbppbpp++&aQbpppbpbppbpbpb:.ppbpppW\n"+
+                "        dbpbppbpbpppppbbbbpbpppbpbppbppppbpbpbpbW\n"+
+                "  `   `.pppbpppbpbpbpppppppbbpbppbppbpbppbppbpppW\n"+
+                "      .bbpbpbpbpppbpbbpbppbppbppbpWkkkbpbppbppbpW\n"+
+                "    ` JbpbpppbNNNNNgkbppbppbppbWNMM#\"MMMNkpbpbppW\n"+
+                "    .XpbppbpWNMMMMHHMMNpbppbbNMM\"!     .WMNppbppW\n"+
+                "    dppbppbNM#^      dMHpbppMM]          ?MNbbpbW\n"+
+                "   .bpppbWMM$        dMdpbpbMMN,         .M#ppppW\n"+
+                "    WbppbMM%        .MMpbppbbMMMNJ.     .dMNpbppW\n"+
+                "    ,bpbbMMb      .MMMpbppbppbpbMMMN&..&MMHppbpbW\n"+
+                "    dpbppbHMMNg++kMMbppbpbppbppbpbbHMMMMHbppbpppW\n"+
+                "    ppppbppppWHMMHbbbpppbppbppbppppppppbpppbpbppW\n"+
+                "    WpbppbppbppbppbpbbpbppbppbpbpbpbbpbpbpbppbpbW\n"+
+                "     .4pbppbpppbpbpbppbpbppbpbppbpbppbpbppbppbppW\n"+
+                "       ?WppbpbppbpbpbppbppbppbpbppbpbppbppbppWY!,\n"+
+                "         _7UbpbppppbpbppbppbppbppbppbppbpbV=`   .\n"+
+                "               ?74bppbppbppbppbpbppbpbppb)      .\n",
+
+
+
+                "                 .Wpbpk,       .(pbpbp;         ,\n"+
+                "                .bpppbp$      .pppppppP         ,\n"+
+                "               .bpbpbpp\\      ppbbpbpbW         ,\n"+
+                "       `      .WppbppbW`     .ppbppbppp)        ,\n"+
+                "    `         dbpppbpbf   `  Jbppbppbpb]         \n"+
+                "          `  dbpbpbppp%     .bbppbppbppb         \n"+
+                "           .bbpbppbppK    ` dbpbppbppbpW         \n"+
+                "    `      ,bppbppbpbf     .bppbppbpbppb-        \n"+
+                "       `  `dbppbpbpbb$     Jbppbpbppbppp[        \n"+
+                "         `(ppbppbpppb\\    .bbpbppbppbpbp]       \n"+
+                "  `      .pppbppbpbpp:  ` .bppbppbpbpppb]        \n"+
+                "     `   dppbpbppbpbp`    dbpbpbppbpbppp\\ ppppbpW\n"+
+                "        .bbppbpbppbpp++&aQbpppbpbppbpbpb:.ppbpppW\n"+
+                "        dbpbppbpbpppppbbbbpbpppbpbppbppppbpbpbpbW\n"+
+                "  `   `.pppbpppbpbpbpppppppbbpbppbppbpbppbppbpppW\n"+
+                "      .bbpbpbpbpppbpbbpbppbppbppbpWkkkbpbppbppbpW\n"+
+                "    ` JbpbpppbNNNNNgkbppbppbppbWNMM#\"MMMNkpbpbppW\n"+
+                "    .XpbppbpWNMMMMHHMMNpbppbbNMM\"!     .WMNppbppW\n"+
+                "    dppbppbNM#^      dMHpbppMM]          ?MNbbpbW\n"+
+                "   .bpppbWMM$        dMdpbpbMMN,         .M#ppppW\n"+
+                "    WbppbMM%        .MMpbppbbMMMNJ.     .dMNpbppW\n"+
+                "    ,bpbbMMb      .MMMpbppbppbpbMMMN&..&MMHppbpbW\n"+
+                "    dpbppbHMMNg++kMMbppbpbppbppbpbbHMMMMHbppbpppW\n"+
+                "    ppppbppppWHMMHbbbpppbppbppbppppppppbpppbpbppW\n"+
+                "    WpbppbppbppbppbpbbpbppbppbpbpbpbbpbpbpbppbpbW\n"+
+                "     .4pbppbpppbpbpbppbpbppbpbppbpbppbpbppbppbppW\n"+
+                "       ?WppbpbppbpbpbppbppbppbpbppbpbppbppbppWY!,\n"+
+                "         _7UbpbppppbpbppbppbppbppbppbppbpbV=`   .\n"+
+                "               ?74bppbppbppbppbpbppbpbppb)      .\n",
+
+
+                "                 .Wpbpk,       .(pbpbp;         ,\n"+
+                "                .bpppbp$      .pppppppP         ,\n"+
+                "               .bpbpbpp\\      ppbbpbpbW         ,\n"+
+                "       `      .WppbppbW`     .ppbppbppp)        ,\n"+
+                "    `         dbpppbpbf   `  Jbppbppbpb]    ....,\n"+
+                "          `  dbpbpbppp%     .bbppbppbppb  .pppppW\n"+
+                "           .bbpbppbppK    ` dbpbppbppbpW  (ppbbpW\n"+
+                "    `      ,bppbppbpbf     .bppbppbpbppb- .bpppbW\n"+
+                "       `  `dbppbpbpbb$     Jbppbpbppbppp[ JbbpppW\n"+
+                "         `(ppbppbpppb\\    .bbpbppbppbpbp] XppbppW\n"+
+                "  `      .pppbppbpbpp:  ` .bppbppbpbpppb] WppbpbW\n"+
+                "     `   dppbpbppbpbp`    dbpbpbppbpbppp\\ ppppbpW\n"+
+                "        .bbppbpbppbpp++&aQbpppbpbppbpbpb:.ppbpppW\n"+
+                "        dbpbppbpbpppppbbbbpbpppbpbppbppppbpbpbpbW\n"+
+                "  `   `.pppbpppbpbpbpppppppbbpbppbppbpbppbppbpppW\n"+
+                "      .bbpbpbpbpppbpbbpbppbppbppbpWkkkbpbppbppbpW\n"+
+                "    ` JbpbpppbNNNNNgkbppbppbppbWNMM#\"MMMNkpbpbppW\n"+
+                "    .XpbppbpWNMMMMHHMMNpbppbbNMM\"!     .WMNppbppW\n"+
+                "    dppbppbNM#^      dMHpbppMM]          ?MNbbpbW\n"+
+                "   .bpppbWMM$        dMdpbpbMMN,         .M#ppppW\n"+
+                "    WbppbMM%        .MMpbppbbMMMNJ.     .dMNpbppW\n"+
+                "    ,bpbbMMb      .MMMpbppbppbpbMMMN&..&MMHppbpbW\n"+
+                "    dpbppbHMMNg++kMMbppbpbppbppbpbbHMMMMHbppbpppW\n"+
+                "    ppppbppppWHMMHbbbpppbppbppbppppppppbpppbpbppW\n"+
+                "    WpbppbppbppbppbpbbpbppbppbpbpbpbbpbpbpbppbpbW\n"+
+                "     .4pbppbpppbpbpbppbpbppbpbppbpbppbpbppbppbppW\n"+
+                "       ?WppbpbppbpbpbppbppbppbpbppbpbppbppbppWY!,\n"+
+                "         _7UbpbppppbpbppbppbppbppbppbppbpbV=`   .\n"+
+                "               ?74bppbppbppbppbpbppbpbppb)      .\n"
+            };
+            Console.WriteLine (koby[chinko]+
+"＿人人人人人人人人人人人人人人人人人人人人人人＿\n" +
+"＞　　助けてください！何でもしますから！　　　＜\n" +
+"￣Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y￣\n" +
+"-----------------------------------------------\n" +
+"タイピングでコビーを去勢の危機から救え！");
+
+
+            string[] mondai = new string[] {"核爆弾","総合開催所","ガバの妖精","かえるの金","組長","バカロ","カサカサ","フライゴン","s102くらいありそう"};
+            for (int i=0; i < 9; i++) 
+            {
+                bool gameover = false;
+                Console.WriteLine(mondai[i]);
+                string input = Console.ReadLine();
+
+                if (mondai[i] == input)
+                {
+                    Console.WriteLine("やりますねえ！");
+                }
+                else
+                {
+                    chinko--;
+                    switch (chinko)
+                    {
+                        case 2:
+                            Console.WriteLine("ガバガバかよ！\n" +
+                                koby[chinko] +
+                                "＿人人人人人人人人人人人人人人人人人人人人人人人人＿\n" +
+                                "＞　　何しやがる！ニホンジンになったじゃねえか！　　　＜\n" +
+                                "￣Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y￣\n");
+                            break;
+
+                        case 1:
+                            Console.WriteLine("ガバガバかよ！\n" +
+                                koby[chinko] +
+                                "＿人人人人人人人人人人人人人人人人人人人人人人人＿\n" +
+                                "＞　　ふざけんな！こっちは将来がかかってんだよ!＜\n" +
+                                "￣Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y￣\n");
+                            break;
+
+                        case 0:
+                            gameover = true;
+                            break;  
+                    }
+                }
+                if (gameover)//ifはtrueだったら実装するのでif(gameover==true)にしなくてよい
+                {
+                    Console.WriteLine("ガバガバかよ！\n" + 
+                        koby[chinko] +
+                        "＿人人人人人人人人人人人人人人人人人人人人人人人＿\n" +
+                        "＞　　お前絶対に許さんからな？覚えてろよ？　　　＜\n" +
+                        "￣Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y￣\n" +
+                        "ゲームオーバー！！");
+                    Console.ReadKey();
+                    break;
+                }
+            }
+            switch (chinko)
+            {
+                case 3:
+                    Console.WriteLine(koby[chinko] +
+                        "人人人人人人人人人人人人人人人人人人人＿\n" +
+                        "＞　　助かったよ！本当にありがとう！　　＜\n" +
+                        "￣Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^￣\n" +
+                        "コビーは無事に脱出できた！！ゲームクリア！！");
+                Console.ReadKey();
+                    break;
+
+                case 2:
+                case 1:
+                    Console.WriteLine(koby[chinko] +
+                        "人人人人人人人人人人人人人人人人人人人人人人人人人人人人人人人人人人人人人人人人人人人人人人＿\n" +
+                        "＞　　俺がサンホンジンじゃなきゃお前恨まれてたぞ！俺は寛大だから許すけどな！ありがとう！　　＜\n" +
+                        "￣Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^￣\n" +
+                        "コビーは何とか脱出できた！！ゲームクリア！！");
+                    Console.ReadKey();
+                    break;
+            }
+        }
+    }
+}
+
